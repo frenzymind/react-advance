acab3f16fa8cabd6e562baaff0757284f83b2dc4
Nikolai <frenzymind@mail.ru>
2022-10-21 01:36:05 +0800

    FSD apply

---
+++ src/app/App.tsx
@@ -0,0 +1,27 @@
+import { Suspense } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import './styles/index.scss'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { useTheme } from 'shared/providers/ThemeProvider'
+import { AboutPage } from 'pages/AboutPage'
+import { MainPage } from 'pages/MainPage'
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to='/'>Main</Link>
+      <Link to='/about'>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPage />} />
+          <Route path={'/'} element={<MainPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
+import App from 'app/App'
+import { ThemeProvider } from 'shared/providers/ThemeProvider'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import ThemeProvider from './theme/ThemeProvider'
 
 render(
   <BrowserRouter>
