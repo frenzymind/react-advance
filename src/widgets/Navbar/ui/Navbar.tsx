89cddca43e99decae91eb4269c503601fe52c493
Nikolai <frenzymind@mail.ru>
2022-11-04 22:18:35 +0800

    fix screens refs, rid unused vars

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,4 @@
 import { classNames } from 'shared/lib/classNames/classNames'
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
 import cls from './Navbar.module.scss'
 
 interface NavbarProps {
