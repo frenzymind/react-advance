89c77978b2894191639776d4150f03fad2734cbd
Nikolai <frenzymind@mail.ru>
2022-11-23 17:45:20 +0800

    add dropdown update navbar, add avatar

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,4 @@
 import { useTranslation } from 'react-i18next'
-import { ListBox } from 'shared/ui/ListBox/ListBox'
-import { HStack } from 'shared/ui/Stack'
 import { Page } from 'widgets/Page/Page'
 
 const MainPage = () => {
