395506d55b81d602f3e96d056c5e6bc84d307503
Nikolai <frenzymind@mail.ru>
2022-12-14 23:02:41 +0800

    fix testing public api

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,12 +1,10 @@
-/* eslint-disable test-fsd/public-api-imports */
-// TODO: Fix testing public API
 import { Story } from '@storybook/react'
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider'
-import { articleDetailsReducer } from '@/entities/Article/model/slice/articleDetailsSlice'
-import { addCommentFormReducer } from '@/features/AddCommentForm/model/slices/addCommentFormSlice'
-import { loginReducer } from '@/features/AuthByUsername/model/slice/loginSlice'
-import { profileReducer } from '@/features/EditableProfileCard/model/slice/profileSlice'
-import { articleDetailsPageReducer } from '@/pages/ArticleDetailsPage/model/slice'
+import { articleDetailsReducer } from '@/entities/Article/testing'
+import { addCommentFormReducer } from '@/features/AddCommentForm/testing'
+import { loginReducer } from '@/features/AuthByUsername/testing'
+import { profileReducer } from '@/features/EditableProfileCard/testing'
+import { articleDetailsPageReducer } from '@/pages/ArticleDetailsPage/testing'
 import { ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
 
 const defaultAsyncReducers: ReducersList = {
