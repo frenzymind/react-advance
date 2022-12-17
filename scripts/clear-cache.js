10628cc85b132a5468ef77a30b8de6ed675b8811
Nikolai <frenzymind@mail.ru>
2022-12-17 13:15:49 +0800

    delete cache folder script

--- scripts/clear-cache.js
+++ scripts/clear-cache.js
@@ -1,2 +1,12 @@
-// TODO: CLear cache
-console.log('CLEAR CACHE')
+const fs = require('fs')
+const path = require('path')
+
+const dir = path.resolve(__dirname, '..', 'node_modules', '.cache')
+
+fs.rm(dir, { recursive: true, force: true }, err => {
+    if (err) {
+        throw err
+    }
+
+    console.log(`CACHE DELETED: ${dir}`)
+})
