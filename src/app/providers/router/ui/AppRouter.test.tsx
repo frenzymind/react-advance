a78bc2295ecdbf5098b87aec62fcd6d2d447b8ee
Nikolai <frenzymind@mail.ru>
2022-12-21 00:00:30 +0800

    add lint staged

--- src/app/providers/router/ui/AppRouter.test.tsx
+++ src/app/providers/router/ui/AppRouter.test.tsx
@@ -2,9 +2,9 @@ import { screen } from '@testing-library/react'
 
 import { AppRouter } from './/AppRouter'
 
+import { UserRole } from '@/entities/User'
 import { getRouteAbout, getRouteAdmin, getRouteProfile } from '@/shared/constants/router'
 import { componentRender } from '@/shared/lib/tests/componentRender/componentRender'
-import { UserRole } from '@/entities/User'
 
 describe('app/router/AppRouter', () => {
     test('Page should be rendered', async () => {
